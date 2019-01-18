a124cc8452a394a3f36fdcfe0b5931f653a5d6c7
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-01-18 11:08:26 -0800

    add specs

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,1 +1,32 @@
+describe('Search', function () {
 
+  it('will the medical issue provides a list of doctors', function() {
+    expect(medicalIssue).toEqual(15);
+  });
+
+  it('will the name of a doctore provides a list of doctors', function() {
+    let doctorName = Bob
+    expect(nameMatch).toEqual(5);
+  });
+
+  it('will the name of a doctore provides a list of doctors', function() {
+    let doctorName = Bob
+    expect(fistnameMatch).toEqual(placeholder);
+    expect(lastnameMatch).toEqual(placeholder);
+    expect(addressMatch).toEqual(address);
+    expect(phonenumberMatch).toEqual(234-456-7890);
+    expect(newpatientMatch).toEqual(Yes);
+  });
+
+  it('will return an error message if API is not sucessful', function() {
+    let doctorName = Bob
+    expect(nameMatch).toEqual(errormassage);
+  });
+
+  it('will return a message is there are no match for the search', function() {
+    let doctorName = Bob
+    expect(nameMatch).toEqual('there are no match for this search in your area');
+  });
+
+
+});
