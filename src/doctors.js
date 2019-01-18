ae23392f586054569024d0c8087d1f99638450e0
Student <example@example.com>
2019-01-18 14:56:14 -0800

    was able to seperate logic

---
+++ src/doctors.js
@@ -0,0 +1,24 @@
+export class Search {
+  searchDoctors(lon, lat) {
+    return new Promise(function(resolve,reject){
+      let key = process.env.API_KEY;
+      let request = new XMLHttpRequest();
+      // let location="37.773,-122.413"
+
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${lon},${lat},100&skip=2&limit=10&user_key=${key}`;
+      request.onload = function(data) {
+        if (this.status === 200) {
+          resolve(this.response)
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  searchResults(){
+    this.response
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,19 +2,32 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Search } from './../src/doctors.js';
 
 $(document).ready(function() {
-  var api_key = process.env.API_KEY;
 
-  var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${api_key}`;
+  var lon = 37.773;
+  var lat = -122.413;
+  console.log(lon);
+  console.log(lat);
 
+  let doctorResults = new Search();
+  let promise = doctorResults.searchDoctors(lon,lat);
 
-
-  $.get(resource_url, function (data) {
-      // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-      var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-      document.getElementById('content-placeholder').innerHTML = template(data);
+  promise.then(function(response){
+    let body = JSON.parse(response);
+    $('.docs').text(`API call went through, now we need to get the data ${body}`);
+  }, function(error){
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
-
+  // var api_key = process.env.API_KEY;
+  // var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${lon},${lat},100&skip=2&limit=10&user_key=${api_key}`;
+  //
+  // $.get(resource_url, function (data) {
+  //     // data: { meta: {<metadata>}, data: {<array[Practice]>} }
+  //     var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+  //     document.getElementById('content-placeholder').innerHTML = template(data);
+  // });
+  //
 
 });
