46aac1707490076a06e673d1ebc1450707cca172
Student <example@example.com>
2019-01-18 19:12:50 -0800

    dynamically displayed doctor's name

--- src/main.js
+++ src/main.js
@@ -3,85 +3,102 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Search } from './../src/doctors.js';
-import { Conditions } from './../src/conditions.js';
+// import { Conditions } from './../src/conditions.js';
 
 $(document).ready(function() {
 
-  var lon = 37.773;
-  var lat = -122.413;
-  console.log(lon);
-  console.log(lat);
-  var condition = "Acne";
+  $('.findDoctors').submit(function(event){
+    event.preventDefault();
+    let providerInput = $('#provider').val();
+    $('#provider').val("");
+    console.log(providerInput)
 
-  let conditionResults = new Conditions();
-  let promise = conditionResults.searchConditions();
+    var lon = 45.5423657;
+    var lat = -122.9345683;
+    let doctorResults = new Search();
+    let promiseDoctor = doctorResults.searchDoctors(lon,lat);
 
-  promise.then(function(response){
-    let condition = JSON.parse(response);
-    let x = [];
-    var i = 0;
-    var j = 0;
-    for(i in condition.data) {
-        x = condition.data[i].name;
-        console.log(x);
-    }
-    $('.conditions').text(`API call went through, now we need to get the symptoms: ${x}`);
-  }, function(error){
-    $('.showErrorsCondition').text(`There was an error processing your request: ${error.message}`);
-  });
+    promiseDoctor.then(function(response){
+      let body = JSON.parse(response);
+      let x = [];
+      var i = 0;
+      var j =0;
+      let y = [];
+
+      var newHTML = [];
+      // for(i in body.data) {
+      //   for (j in body.data[i].profile){
+      //     x = body.data[i].profile.first_name;
+      //     newHTML.push('<li>' + x + '</li>');
+      //   }
+      // }
+
+      for(i in body.data) {
+          x = body.data[i].profile.first_name;
+          y = body.data[i].profile.last_name;
+          newHTML.push('<li>' + x + " " + y + '</li>');
+        }
+
+
+
+      $(".result").html(newHTML.join(""));
+
+      // for(i in body.data) {
+      //   for (j in body.data[i].specialties){
+      //     x = body.data[i].specialties[j].name;
+      //     console.log(x);
+      //   }
+      // }
+    }, function(error){
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  })
+
+  // var lon = 37.773;
+  // var lat = -122.413;
+  // console.log(lon);
+  // console.log(lat);
+  // var condition = "Acne";
+  //
+  // let conditionResults = new Conditions();
+  // let promise = conditionResults.searchConditions();
+  //
+  // promise.then(function(response){
+  //   let condition = JSON.parse(response);
+  //   let a = [];
+  //   var b = 0;
+  //   for(b in condition.data) {
+  //       a = condition.data[b].name;
+  //       // console.log(a);
+  //   }
+  //   $('.conditions').text(`API call went through, now we need to get the symptoms: ${a}`);
+  // }, function(error){
+  //   $('.showErrorsCondition').text(`There was an error processing your request: ${error.message}`);
+  // });
 
 
 // ///Doctors//////
-//   let doctorResults = new Search();
-//   let promise = doctorResults.searchDoctors(lon,lat);
-//
-//   promise.then(function(response){
-//     let body = JSON.parse(response);
-//     let x = body.data[0];
-//     var i = 0;
-//     var j =0;
-//     var k = 0;
-//     let y = body.data[i].profile
-//     // for(i in body.data) {
-//     //   x = body.data[i];
-//     //   console.log(x);
-//     // }
-//
-//     for(i in body.data) {
-//       for (j in body.data[i].profile){
-//         x = body.data[i].profile[j];
-//         console.log(x);
-//       }
-//     }
-//     y.forEach(function(data){
-//       console.log(data);
-//     })
-//
-//     // for(i in body.data) {
-//     //   for (j in body.data[i].profile){
-//     //     for (k in body.data[i].profile[j].bio){
-//     //       x = body.data[i].profile[j].bio[k];
-//     //       console.log(x);
-//     //     }
-//     //   }
-//     // }
-//
-//
-//     $('.docs').text(`API call went through, now we need to get the data ${body.data}`);
-//   }, function(error){
-//     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-//   });
-
-
-  //delete when done:
-  // var api_key = process.env.API_KEY;
-  // var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${lon},${lat},100&skip=2&limit=10&user_key=${api_key}`;
+  // let doctorResults = new Search();
+  // let promiseDoctor = doctorResults.searchDoctors(lon,lat);
   //
-  // $.get(resource_url, function (data) {
-  //     // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-  //     var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-  //     document.getElementById('content-placeholder').innerHTML = template(data);
+  // promiseDoctor.then(function(response){
+  //   let body = JSON.parse(response);
+  //   let x = [];
+  //   var i = 0;
+  //   var j =0;
+  //   let y = y;
+  //
+  //   for(i in body.data) {
+  //     for (j in body.data[i].profile){
+  //       x = body.data[i].profile.first_name;
+  //       y = body.data[i].profile.last_name;
+  //       // console.log(x + " " + y);
+  //     }
+  //   }
+  //
+  //   $('.docs').text(`API call went through, now we need to get the data ${x + " " + y}`);
+  // }, function(error){
+  //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   // });
-  //
 
 });
