cf641d82d8011b9e8f286851e749ce0a229e5cd2
Student <example@example.com>
2019-01-21 14:58:31 -0800

    merge with branch and remove unused classes

--- src/main.js
+++ src/main.js
@@ -3,31 +3,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Search } from './../src/doctors.js';
-import { Conditions } from './../src/conditions.js';
-// import { profile } from './../src/profile.js';
 
 $(document).ready(function() {
-
-  // let conditionResults = new Conditions();
-  // let promiseCondition = conditionResults.searchConditions();
-  //
-  // promiseCondition.then(function(response){
-  //   let bodyCondition = JSON.parse(response);
-  //
-  //   var conditionsArr = []
-  //   var j = 0;
-  //   let x = [];
-  //   var conditionHTML = []
-  //
-  //   for(j in bodyCondition.data) {
-  //     x = bodyCondition.data[j].name;
-  //     // console.log(x);
-  //     conditionHTML.push('<option id="'+ j + '">'+ x + '</option>');
-  //   }
-  //   $('#condition').html(conditionHTML);
-  // }, function(error){
-  //   $('.showErrorsCondition').text(`There was an error processing your request: ${error.message}`);
-  // });
 
   $('.findDoctors').submit(function(event){
     event.preventDefault();
